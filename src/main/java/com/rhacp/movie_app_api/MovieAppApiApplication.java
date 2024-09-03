f7fea0db627f3fc6692d0874d124eafa349e5a86
Anghel Andrei <mirceanghel98@gmail.com>
2024-09-04 02:06:06 +0300

    Start done.

--- src/main/java/com/rhacp/movie_app_api/MovieAppApiApplication.java
+++ src/main/java/com/rhacp/movie_app_api/MovieAppApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieAppApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieAppApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MovieAppApiApplication.class, args);
+    }
 
 }
